e30f863422b0a86e639eeab2521e4ba0977b92cf
Kim Knight <kim@kimknight.net>
2023-09-21 15:54:31 +1000

    Add MongoDB connection. Make routes related to events perform the required operations on the database.

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,53 @@
 const express = require("express")
 const app = express()
 
-// Import express-session and setup to handle session management
+// Activate the "express.urlencoded" feature which allows us to read basic form data submitted by clients.
+// This will be needed to read the username/password submitted from a login page
+app.use(express.urlencoded());
 
-// Import MongoDB and connect to server
+app.use(express.json( { limit: '1mb' } ));
+
+
+// Import the express-session library which will use cookies to keep track of client sessions
+var session = require("express-session");
+// Tell the web server (app) to use express-session to track sessions
+app.use(
+    session({
+        secret: "eNYNRMTeHhNIzqznJ8U8kzgFhqs6aZMbaCpKrE6hXT2rcW",
+
+        // Set the max session time to 10 minutes. 1000 (ms) * 60 (seconds) * 10 (minutes)
+        maxAge: 1000 * 60 * 10,
+    })
+);
+
+
+// Import the MongoDB client library to a new object called "MongoClient" which we will use to access all MongoDB functions
+const {MongoClient} = require("mongodb");
+
+// Define the connection URL of the MongoDB server and additional options.
+// In this case we set the "family" option to 4 which forces the connection to use IPv4 which avoids some issues
+const client = new MongoClient("mongodb://localhost/", { family: 4 })
+
+// The function that will open the connection to the MongoDB server.
+// By using making this an "async" function we can use "await" statements inside of it.
+// In simple terms, an "await" statements tell JavaScript to wait until the operation
+// is complete before continuing
+async function connect() {
+    try {
+        await client.connect();
+        console.log("Successfully connected to MongoDB!");
+    } catch {
+        console.log("Error connecting to MongoDB.")
+    }
+}
+
+// Run the "connect()" function we created above
+connect()
+
+// Define a variable that is a reference to a collection we want to access in the database.
+// If working with multiple collections, you could create a variable for each one
+let usersCollection = client.db("eventpulse").collection("users");
+let eventsCollection = client.db("eventpulse").collection("events");
 
 
 // Route to share the main content folder
@@ -16,16 +60,50 @@ app.use("/", express.static("content"))
 
 app.get("/events", function (req, res) {
     // Client is requesting JSON event list
+
+    eventsCollection.find().toArray()
+    .then((results) => {
+        res.json(results);
+    });
 })
 
 app.post("/events", function (req, res) {
     // Client is attempting to add a new event
-    // Require client to be logged in
+    // Require client to be logged in !!!!!
+
+    let newJob = req.body
+
+    // Insert the new job into the database
+    eventsCollection.insertOne(newJob)
+    .then(() => {
+        // Return the updated joblist
+        eventsCollection.find().toArray()
+        .then((results) => {
+            res.json(results);
+        });
+    });
 })
 
 app.delete("/events", function (req, res) {
     // Client is attempting to delete an event
-    // Require client to be logged in
+    // Require client to be logged in !!!!!
+
+    let eventToDelete = req.body
+
+    // Delete the _id property from the eventToDelete. Leaving it there will cause the job not to be deleted
+    delete eventToDelete._id
+
+    // Delete the job matching eventToDelete from the database
+
+    eventsCollection.deleteOne(eventToDelete)
+    .then(() => {
+        // Return the updated joblist
+        eventsCollection.find().toArray()
+        .then((results) => {
+            res.json(results);
+        });
+    });
+
 })
 
 
